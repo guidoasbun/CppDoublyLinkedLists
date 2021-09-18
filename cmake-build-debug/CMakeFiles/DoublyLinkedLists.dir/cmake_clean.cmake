70b5245b9914b8e020e4650dd2214bfa7f1c6ac1
guidoasbun <guido@asbun.io>
2021-09-18 13:04:51 -0700

    Initial

---
+++ cmake-build-debug/CMakeFiles/DoublyLinkedLists.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DoublyLinkedLists.dir/main.cpp.o"
+  "DoublyLinkedLists"
+  "DoublyLinkedLists.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DoublyLinkedLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
